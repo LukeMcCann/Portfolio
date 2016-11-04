c8dfb010cd3fd8fc0fba945e50c98c2e1af575e2
Luke McCann <Developer-lukemccann@outlook.com>
2016-11-04 14:45:44 +0000

    Perlin Noise Algorithm
    Perlin Noise Algorithm along with Object generator (terrain)

---
+++ Perlin_Noise_Algorithm/PerlinNoise.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerlinNoise {
+	
+	long seed;
+	
+	public PerlinNoise(long seed)
+    {
+		this.seed = seed;
+	}
+	
+    /*
+     * random - returns a randomly generated seed of a certain range.
+     * @param int x - minimum to int start at.
+     * @param int range - maximum possible value.
+     */
+	private int random(long x, int range)
+    {
+		return (int)(((x+seed)^5) % range);
+	}
+	
+    /**
+     * getNoise - Returns a random int between the chunkSize and Range
+     * @param int x - minimum int to start at.
+     * @param int range - the maximum possible value.
+     */
+	public int getNoise(int x, int range)
+    {
+		int chunkSize = 16;
+		float noise = 0;
+		
+		range /= 2;
+		
+		while(chunkSize > 0){
+			int chunkIndex = x / chunkSize;
+			float prog = (x % chunkSize) / (chunkSize * 1f);
+			float lRandom = random(chunkIndex, range);
+			float rRandom = random(chunkIndex + 1, range);
+			
+			
+			noise += (1-prog) * lRandom + prog * rRandom;
+			
+			chunkSize /= 2;
+			range /= 2;
+			
+			range = Mathf.Max(1,range);
+		}
+		
+		return (int)Mathf.Round(noise);
+	}
+}
---
+++ Perlin_Noise_Algorithm_1Dimensional/Generators/Generator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	
+	#pragma warning disable 78
+	
+	public GameObject dirtPrefab; 
+	public GameObject grassPrefab; 
+	
+	int minX = -16;
+	int maxX = 16;
+	int minY = -10;
+	int maxY = 10;
+	
+	PerlinNoise noise;
+	
+	void Start ()
+    {
+		noise = new PerlinNoise(Random.Range(1000000,10000000));
+		Regenerate();
+	}
+	
+    /*
+     * Generates a random 2D terrain.
+     */
+	private void Regenerate()
+    {
+		
+		float width = dirtPrefab.transform.localScale.x;
+		float height = dirtPrefab.transform.localScale.y;
+		
+		for (int i = minX; i < maxX; i++)//iterate columns (x values)
+        {
+			int columnHeight = 2 + noise.getNoise(i - minX, maxY - minY - 2); 
+            for (int j = minY; j < minY + columnHeight; j++)//iterate rows (y values)
+            {
+				GameObject tile = (j == minY + columnHeight - 1)?grassPrefab:dirtPrefab;
+				Instantiate(tile, new Vector2(i * width, j * height), Quaternion.identity);
+			}
+		}
+	}
+}
